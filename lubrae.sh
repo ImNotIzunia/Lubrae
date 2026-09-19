460ac13f9bd1fbe183f006632b0875d06e49fac5
ImNotIzunia <charles.berthet@lamache.org>
2026-09-19 18:21:55 +0200

    Add Set-Format

--- lubrae.sh
+++ lubrae.sh
@@ -105,6 +105,65 @@ Set-Loops() {
 
 
 # SYNOPSIS
+# Set Disk Format
+#
+# DESCRIPTION
+# Ask the user what kind of 
+# format he wants after the wipe
+#
+# EXAMPLE
+# Set-Format
+#
+# OUTPUTS
+# None
+#
+Set-Format() {
+    local value
+
+    echo ""
+
+    echo "1. none"
+    echo "2. ext4"
+    echo "3. xfs"
+    echo "4. vfat (FAT32)"
+    echo "5. exfat"
+    echo "6. ntfs"
+
+    echo ""
+
+    while true; do
+        read -rp "Format type (emptuy to cancel): " value || exit 0
+
+        case "$value" in
+            1)
+            FORMAT=""; return ;;
+            
+            2)
+            FORMAT="ext4"; return ;;
+
+            3)
+            FORMAT="xfs"; return ;;
+
+            4)
+            FORMAT="vfat"; return ;;
+
+            5)
+            FORMAT="exfat"; return ;;
+
+            6)
+            FORMAT="ntfs"; return ;;
+
+            "")
+            return ;;
+
+            *)
+            echo "Invalid choice. Please try again..." ;;
+        esac
+    done
+}
+
+
+# SYNOPSIS
 # Display the header
 #
 # DESCRIPTION
@@ -172,7 +231,7 @@ Show-Menu() {
 
             3) 
                 clear
-                echo "format" 
+                Set-Format
             ;;
 
             4) 
